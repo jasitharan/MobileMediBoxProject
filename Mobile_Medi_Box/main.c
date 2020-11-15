1ef176c7455ca10dbb1aa3c391d4fa18af9a7b86
Jasitharan <jasitharan1998@gmail.com>
2020-11-15 15:38:00 +0530

    normal commit

--- Mobile_Medi_Box/main.c
+++ Mobile_Medi_Box/main.c
@@ -5,7 +5,7 @@
  * Author : jasitharan
  */ 
 
-#define F_CPU 8000000UL
+#define F_CPU 8000000UL // Define CPU Frequency 8MHz
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
@@ -14,43 +14,34 @@
 #include "i2cmaster.h"
 #include "i2c_lcd.h"
 
-#define TEMP_PORT_PIN 0b10000000
-#define OXY_PORT_PIN 0b00100000
-#define HEART_PORT_PIN 0b00010000
-#define ECG_PORT_PIN 0b00001000
+
+// Servo Motors Pins
+#define TEMP_SENSOR 0b00000000
+#define OXY_SENSOR 0b00000010
+#define HEART_SENSOR 0b00000001
+#define ECG_SENSOR 0b00000011
 
 int age = 0;  // Age
 int gender = 0;  // Gender
 int num = 0;
 Keypad key;
-char *result = "72bpm"; // Final Result
-int selected = -1;
 
-int *p1 = &PORTD;
-int *p2 = &PORTB;
+
+
 
 
 void getAge();
 void getGender();
 void sensor();
-void sensorInstruction(int n,char *c);
-void resultFunc();
+void sensorInstruction(char *c);
 
-void pushSensor(int *port,int n);
-void pullSensor(int *port,int n);
+
+void pushSensor(int n);
+void pullSensor(int n);
 
 
 int main(void)
 {
-	//OC For Servo Motors
-	DDRD = 0b10110000; //make output oc pins
-	DDRB = 0b00001000; //make output oc pins
-	
-	PORTD = 0x00;
-	PORTB = 0x00;
-	
-
-	
 	
 	keypad_init();                                  // KEYPAD Initialize
 	lcd_init(LCD_BACKLIGHT_ON);                     // LCD Initialize
@@ -58,20 +49,19 @@ int main(void)
 	
 	lcd_clear();			/* Clear LCD */
 	_delay_ms(2000);
-	lcd_goto_xy(0,1);		/* Enter column and row position */
+	lcd_goto_xy(0,0);		/* Enter column and row position */
 	lcd_puts("WELCOME...." );
 	_delay_ms(2000);
-	lcd_puts_at("Mobile Medi Box",1,1);
+	lcd_puts_at("Mobile Medi Box",0,1);
 	_delay_ms(2000);
 	lcd_clear();
 	_delay_ms(300);
 	
 	while (1)
 	{
-		getAge();
-		getGender();
-		sensor();
-		if (selected != -1) resultFunc();
+		getAge();                         // For Getting Age
+		getGender();                      // For Getting Gender
+		sensor();                         // For Sensor Selection And Functions
 	}
 }
 
@@ -83,10 +73,10 @@ void getAge()
 	int i = 0;
 	lcd_clear();
 	_delay_ms(100);
-	lcd_goto_xy(0,1);
+	lcd_goto_xy(0,0);
 	lcd_puts("Age: ");
 	_delay_ms(100);
-	lcd_goto_xy(1,1);
+	lcd_goto_xy(0,1);
 	while(1){
 		char *numString;
 		key = waitForKeyPress();
@@ -97,11 +87,11 @@ void getAge()
 			break;
 		}
 		sprintf(numString,"%d",keypad_get_number(key));
-		num *= 10;
+		num *= 10;      // num = 0 initial value
 		num += keypad_get_number(key);
 		lcd_puts(numString);
 		_delay_ms(100);
-		lcd_goto_xy(1,i++);
+		lcd_goto_xy(i++,1);
 	}
 }
 
@@ -109,9 +99,9 @@ void getAge()
   void getGender()
   {
 	  _delay_ms(100);
-	  lcd_goto_xy(0,1);
+	  lcd_goto_xy(0,0);
 	  lcd_puts("Gender: ");
-	  lcd_puts_at("0.Male 1.Female",1,1);
+	  lcd_puts_at("0.Male 1.Female",0,1);
 	  key = waitForKeyPress();
 	  if (key == BTN_0) gender = 0;
 	  if (key == BTN_1) gender = 1;
@@ -123,42 +113,53 @@ void getAge()
   void sensor()
   {
 	  _delay_ms(100);
-	  lcd_goto_xy(0,1);
+	  lcd_goto_xy(0,0);
 	  lcd_puts("Select: ");
-	  lcd_puts_at("0.T 1.O 2.E 3.H",1,1);
+	  lcd_puts_at("0.T 1.O 2.E 3.H",0,1);
 	  key = waitForKeyPress();
 	  switch(key)
 	  {
 		  case BTN_0:
-		  sensorInstruction(0,"Place Your Finger");
+		  sensorInstruction("Place Your Finger");
 		  // Push The Sensor code
-		  pushSensor(p1,TEMP_PORT_PIN);
+		  pushSensor(TEMP_SENSOR);
 		  
 		  // Temperature Sensor Code 
 		  
+		  
+		  _delay_ms(10000);
+		  pushSensor(TEMP_SENSOR);
 		  break;
 		  case BTN_1:
-		  sensorInstruction(1,"Place Your Finger");
+		  sensorInstruction("Place Your Finger");
 		  // Push The Sensor code
-		  pushSensor(p1,OXY_PORT_PIN);
+		  pushSensor(OXY_SENSOR);
 		  // Oxygen Saturation Code
 		  
+		   _delay_ms(10000);
+		   pushSensor(OXY_SENSOR);
 		  break;
 		  case BTN_2:
-		  sensorInstruction(2,"Place Your Thump");
+		  sensorInstruction("Place Your Thump");
 		  // Push The Sensor code
-		  pushSensor(p2,ECG_PORT_PIN);
+		  pushSensor(ECG_SENSOR);
 		  
 		  // ECG Code
 		  
+		  
+		   _delay_ms(10000);
+		   pushSensor(ECG_SENSOR);
 		  break;
 		  case BTN_3:
-		  sensorInstruction(3,"Put it on your chest");
+		  sensorInstruction("Put it on your chest");
 		  // Push The Sensor code
-		  pushSensor(p1,HEART_PORT_PIN);
+		  pushSensor(HEART_SENSOR);
 		  
 		  // Heart Rate Code
 		  
+		  
+		   _delay_ms(10000);
+		   pushSensor(HEART_SENSOR);
 		  break;
 	      default:
 		  lcd_puts_at("Wrong input",0,0);
@@ -168,77 +169,46 @@ void getAge()
 	  }
   }
   
-  void sensorInstruction(int n,char *c)
+  void sensorInstruction(char *c)
   {
-	  selected = n;
 	  lcd_clear();
 	  _delay_ms(100);
-	  lcd_goto_xy(0,1);
+	  lcd_goto_xy(0,0);
 	  lcd_puts(c);
   }
   
-  void resultFunc()
+  
+
+  
+  
+  
+  void pushSensor(int n)
   {
-	  _delay_ms(2000);
-	  lcd_clear();
-	  lcd_goto_xy(0,1);
-	  lcd_puts("Result: ");
-	  lcd_puts_at(result,1,1);
-	  _delay_ms(10000);
-	  switch(selected)
-	  {
-		  case 0:
-		  pullSensor(p1,TEMP_PORT_PIN);
-		  break;
-		  case 1:
-		  pullSensor(p1,OXY_PORT_PIN);
-		  break;
-		  case 2:
-		  pullSensor(p2,ECG_PORT_PIN);
-		  break;
-		  case 3:
-		  pullSensor(p1,HEART_PORT_PIN);
-		  break;
-		  default:
-		  break;
-		   
-	  }
-	  
-  }
-  
-  
-  
-  void pushSensor(int *port,int n)
-  {
-	  //Rotate Motor to 0 degree
-	  *port = n;
-	  _delay_us(1000);
-	  *port = 0x00;
-	  
-	  _delay_ms(500);
-	  
-	  //Rotate Motor to 90 degree
-	  *port = n;
-	  _delay_us(1500);
-	  *port = 0x00;
-	  
-	  _delay_ms(500);
-	  
-	  //Rotate Motor to 180 degree
-	  *port = n;
-	  _delay_us(2000);
-	  *port = 0x00;
-	  
-	  _delay_ms(500);
+	 PORTD = n;
+	 DDRD |= 0b00100011;
+     TCCR1A |= 1<<WGM11 | 1<<COM1A1;
+     TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS10 | 1<<CS11;
+     ICR1 = 2499;
+
+     OCR1A = 65;	/* Set servo shaft at -90° position */
+    _delay_ms(1500);
+     OCR1A = 175;	/* Set servo shaft at 0° position */
+    _delay_ms(1500);
+     OCR1A = 300;	/* Set servo at +90° position */
+    _delay_ms(1500);
 	  
   }
 
-  void pullSensor(int *port,int n)
+  void pullSensor(int n)
   {
 	  
-	  //Rotate Motor to 0 degree
-	  port = n;
-	  _delay_us(1000);
-	  port = 0x00;
+	  PORTD = n;
+	  DDRD |= 0b00100011;
+	  TCCR1A |= 1<<WGM11 | 1<<COM1A1;
+	  TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS10 | 1<<CS11;
+	  ICR1 = 2499;
+
+	  OCR1A = 65;	/* Set servo shaft at -90° position */
+	  _delay_ms(1500);
 	  
   }
